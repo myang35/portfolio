bf4168e6d75d98e5c612c7d5b87e4848d6897e16
Marvin Yang <marvinyangs@gmail.com>
2026-01-15 13:16:10 -0800

    feat: set base styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div
+	class="flex h-screen flex-col items-center justify-center bg-linear-to-tr from-bg-dark to-highlight p-4"
+>
+	<div class="flex max-w-5xl flex-col gap-4">
+		<h1 class="text-6xl font-bold">Hi, I'm Marvin</h1>
+		<p class="text-3xl">
+			Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+		</p>
+		<p class="text-xl text-text-muted">Documentation here</p>
+		<div class="space-y-2 rounded-xl border bg-bg p-4">
+			<h1 class="border-b border-b-border-muted text-3xl font-bold">Hi There</h1>
+			<p class="text-xl">Just wanted to test how a card would look in this style.</p>
+			<div class="flex items-end gap-4">
+				<input type="text" />
+				<select name="" id="">
+					<option value="">stuff</option>
+					<option value="">stuff2</option>
+					<option value="">stuff3</option>
+				</select>
+				<textarea name="" id=""></textarea>
+			</div>
+		</div>
+		<div class="flex gap-4">
+			<button
+				class="rounded-lg border bg-primary px-3 py-2 text-lg font-bold text-primary-text hover:bg-primary-highlight"
+				>Get in touch</button
+			>
+			<button
+				class="rounded-lg border bg-secondary px-3 py-2 text-lg font-bold text-secondary-text shadow hover:bg-secondary-highlight hover:shadow-md"
+				>Get in touch</button
+			>
+		</div>
+		<div class="flex gap-4">
+			<div
+				class="space-y-2 rounded-xl border bg-danger p-4 text-danger-text shadow-md transition-all hover:-translate-y-1 hover:bg-danger-highlight hover:shadow-lg"
+			>
+				<h1 class="border-b border-b-border-muted text-3xl font-bold">Hi There</h1>
+				<p class="text-xl">Just wanted to test how a card would look in this style.</p>
+			</div>
+			<div
+				class="space-y-2 rounded-xl border bg-warning p-4 text-warning-text shadow-md transition-all hover:-translate-y-1 hover:bg-warning-highlight hover:shadow-lg"
+			>
+				<h1 class="border-b border-b-border-muted text-3xl font-bold">Hi There</h1>
+				<p class="text-xl">Just wanted to test how a card would look in this style.</p>
+			</div>
+			<div
+				class="space-y-2 rounded-xl border bg-success p-4 text-success-text shadow-md transition-all hover:-translate-y-1 hover:bg-success-highlight hover:shadow-lg"
+			>
+				<h1 class="border-b border-b-border-muted text-3xl font-bold">Hi There</h1>
+				<p class="text-xl">Just wanted to test how a card would look in this style.</p>
+			</div>
+			<div
+				class="space-y-2 rounded-xl border bg-info p-4 text-info-text shadow-md transition-all hover:-translate-y-1 hover:bg-info-highlight hover:shadow-lg"
+			>
+				<h1 class="border-b border-b-border-muted text-3xl font-bold">Hi There</h1>
+				<p class="text-xl">Just wanted to test how a card would look in this style.</p>
+			</div>
+		</div>
+	</div>
+</div>
