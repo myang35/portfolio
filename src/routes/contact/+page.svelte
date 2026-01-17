35ffd79e266f3435955888ef81d19857c38fb009
Marvin Yang <marvinyangs@gmail.com>
2026-01-16 17:38:15 -0800

    feat: design contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,42 @@
+<div>
+	<section>
+		<div class="container flex flex-col gap-2 py-16">
+			<h1 class="text-center text-4xl font-bold">Contact Me</h1>
+			<p class="text-center text-xl text-text-muted">
+				Feel free to reach out to me through any of these channels. I'd love to hear from you!
+			</p>
+		</div>
+	</section>
+
+	<section class="border-t">
+		<div class="container mx-auto flex w-fit flex-col gap-8 py-16">
+			<div class="flex items-center gap-4">
+				<span class="icon-[mdi--email] text-2xl"></span>
+				<a href="mailto:marvinyangs@gmail.com" class="transition-colors hover:text-secondary">
+					marvinyangs@gmail.com
+				</a>
+			</div>
+			<div class="flex items-center gap-4">
+				<span class="icon-[mdi--phone] text-2xl"></span>
+				<a href="tel:+18282448582" class="transition-colors hover:text-secondary">
+					+1 (828) 244 - 8582
+				</a>
+			</div>
+			<div class="flex items-center gap-4">
+				<span class="icon-[mdi--github] text-2xl"></span>
+				<a href="https://github.com/myang35" class="transition-colors hover:text-secondary">
+					github.com/myang35
+				</a>
+			</div>
+			<div class="flex items-center gap-4">
+				<span class="icon-[mdi--linkedin] text-2xl"></span>
+				<a
+					href="https://linkedin.com/in/marvin-yang-017b5214b"
+					class="transition-colors hover:text-secondary"
+				>
+					linkedin.com/in/marvin-yang-017b5214b
+				</a>
+			</div>
+		</div>
+	</section>
+</div>
