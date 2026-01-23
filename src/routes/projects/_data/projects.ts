f176a34975b112e339b882e4729bd4a9c871543c
Marvin Yang <marvinyangs@gmail.com>
2026-01-23 11:59:57 -0800

    feat: add more projects and move projects data into its own file

---
+++ src/routes/projects/_data/projects.ts
@@ -0,0 +1,34 @@
+export const projects = [
+	{
+		title: 'Portfolio',
+		description:
+			'A website that introduces who I am as a developer and showcases a selection of my side projects, skills, and experience. The goal of this project is to provide a simple, fast, and accessible way to present my work and make it easy for others to explore the projects I have built.',
+		githubUrl: 'https://github.com/myang35/portfolio.git',
+		websiteUrl: 'https://marvinyang.com',
+		technologies: ['SvelteKit', 'Tailwind CSS', 'TypeScript']
+	},
+	{
+		title: 'MoRecipes',
+		description:
+			'A web application for creating, organizing, and sharing recipes in a simple, visual, and community-friendly way. It’s designed to feel like a digital recipe board — a place where home cooks can save ideas, discover new dishes, and share what they love to make.',
+		githubUrl: 'https://github.com/myang35/taste-board.git',
+		websiteUrl: 'https://morecipes.com',
+		technologies: ['Angular', 'Tailwind CSS', 'Node.js', 'Express.js', 'TypeScript', 'MongoDB']
+	},
+	{
+		title: "Devil's Hexagon",
+		description:
+			'A web game that tests your memory. Play solo or with friends on multiple devices. Have one screen host the game while everyone else joins in from their own phone or computer.',
+		githubUrl: 'https://github.com/myang35/devils-hexagon.git',
+		websiteUrl: 'https://devils-hexagon.netlify.app',
+		technologies: ['SvelteKit', 'Tailwind CSS', 'TypeScript', 'MongoDB']
+	},
+	{
+		title: 'Dodgeball',
+		description:
+			'A simple browser-based, two-player dodgeball game. The goal is to hit the other player with a ball while trying to dodge their balls.',
+		githubUrl: 'https://github.com/myang35/dodgeball.git',
+		websiteUrl: 'https://dodgeball.marvinyang.com',
+		technologies: ['SvelteKit', 'Tailwind CSS', 'TypeScript']
+	}
+];
